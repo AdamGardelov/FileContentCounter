c5be3dbf3f99131657d582a55dcfcd90ba43f543
Adam <adam@gardelov.com>
2023-06-10 17:32:50 +0200

    refactoring: Moved method documentation to interface

--- FileContentCounter/Services/FileService.cs
+++ FileContentCounter/Services/FileService.cs
@@ -4,14 +4,6 @@ namespace FileContentCounter.Services;
 
 public class FileService : IFileService
 {
-    /// <summary>
-    /// Counts the occurrences of a specific word in a file.
-    /// </summary>
-    /// <param name="filePath">The path to the file.</param>
-    /// <param name="fileName">Output parameter that receives the name of the file without extension.</param>
-    /// <returns>The total count of occurrences of the word in the file.</returns>
-    /// <exception cref="ArgumentException">Thrown when the provided filePath is not valid.</exception>
-    /// <exception cref="FileProcessingException">Thrown when an error occurs during file processing.</exception>
     public int CountWordOccurrencesInFile(string filePath, out string fileName)
     {
         if (!Path.Exists(filePath))
